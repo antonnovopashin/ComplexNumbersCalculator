c04b5d1e851959b723a747c8caed413fda6a8456
Anton Novopashin <antonfromkrsk@gmail.com>
2020-02-26 12:25:46 +0300

    phpunit fix

---
+++ tests/NumbersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class NumbersTest extends TestCase
+{
+    public function setUp():void { }
+    public function tearDown():void { }
+
+    /**
+     * @test
+     */
+    public function canSum()
+    {
+        $this->assertTrue(true);
+    }
+}
